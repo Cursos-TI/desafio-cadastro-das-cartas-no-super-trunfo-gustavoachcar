c37a630efc7b3d8838582d423af4a0d143424d38
GUSTAVO ACHCAR <107647983+gustavoachcar@users.noreply.github.com>
2025-08-08 13:53:10 +0000

    variáveis declaradas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,11 +4,16 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Aluno - Gustavo Achcar
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char estado1, estado2, codigo1[5], codigo2[5], nome1[20], nome2[20];
+    int pop1, pop2, num1, num2;
+    float area1, area2, pib1,pib2;
+
+
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
